7a6d5487dca7a610ebf404647940d33d1dbc3f1c
ramsecinfo <124563753+ramsecinfo@users.noreply.github.com>
2024-07-10 22:08:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import *
- 
-app = Flask(__name__)
- 
-@app.route('/')
-def home():
-    return ("<body style='background-color: #9dffff; height=100%; display:flex;'>"
-            "<div style='font-family: Poppins; display:flex; flex-direction: column; align-self: center; width: 100%'>"
-            "<h1 style='text-align:center'>Welcome to Jenkins CI/CD Pipeline for Python Application</h1>"
-            "<h2 style='text-align:center'>Also with SAST Security Scan</h2>"
-            "</div>"
-            "</body>")
- 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+from my_flask_app import app  # Ensure this import matches your app's structure
+import pytest
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_Home(client):
+    response = client.get('/')
+    assert b"<body style='background-color: #9dffff; height=100%; display:flex;'><div style='font-family: Poppins; display:flex; flex-direction: column; align-self: center; width: 100%'><h1 style='text-align:center'>Welcome to Jenkins CI/CD Pipeline for Python Application</h1><h2 style='text-align:center'>Also with SAST Security Scan</h2></div></body>" in response.data
