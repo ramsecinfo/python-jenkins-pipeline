bfca4440334b4d81d9f324508de17feff07e6ee8
ramsecinfo <124563753+ramsecinfo@users.noreply.github.com>
2024-07-10 22:31:52 -0700

    Update testRoutes.py

--- testRoutes.py
+++ testRoutes.py
@@ -1,12 +1,6 @@
-from my_flask_app import app  # Ensure this import matches your app's structure
-import pytest
-
-@pytest.fixture
-def client():
-    with app.app_context():
-        with app.test_client() as client:
-            yield client
-
-def test_Home(client):
-    response = client.get('/')
+from app import *
+ 
+def test_Home():
+    response = app.test_client().get('/')
     assert b"<body style='background-color: #9dffff; height=100%; display:flex;'><div style='font-family: Poppins; display:flex; flex-direction: column; align-self: center; width: 100%'><h1 style='text-align:center'>Welcome to Jenkins CI/CD Pipeline for Python Application</h1><h2 style='text-align:center'>Also with SAST Security Scan</h2></div></body>" in response.data
+    assert response.status_code == 200
